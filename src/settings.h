0b47b8f208899b90a485568663e9a0ef9a3957dd
Mikhail Nitenko <mnitenko@gmail.com>
2021-02-15 22:55:43 +0300

    add idef

--- src/settings.h
+++ src/settings.h
@@ -5,27 +5,21 @@ class settings {
 public:
   double intensity;
 
-  double gamma;
   int cfa_offset[2];
   double raw_white_balance[3] = {1, 1, 1};
 
-  double hue;
   double saturation;
   double brightness;
   double contrast;
-  double vibrance;
 
   settings() {
     this->intensity = 1.0;
-    this->gamma = 0.9;
     this->cfa_offset[0] = 0;
     this->cfa_offset[1] = 0;
 
-    this->hue = 0.0;
     this->saturation = 0.0;
     this->brightness = 0.0;
     this->contrast = 0.0;
-    this->vibrance = 0.0;
   }
 };
 
